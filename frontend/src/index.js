bf4a783f20fa143cf452af4a7d8533b0e95e4a84
yaswanth <aysawnth@enphaseenergy.com>
2024-02-25 09:46:06 +0530

    done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { UserLoginProvider, UserSignUpProvider } from "./context/UserLogin.jsx"
-import { UserListProvider } from './context/UserList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <UserListProvider>
     <UserLoginProvider>
       <UserSignUpProvider>
       <App />
       </UserSignUpProvider>
     </UserLoginProvider>
-    </UserListProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
